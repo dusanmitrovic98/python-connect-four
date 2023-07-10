a944f308d1bef4ed50d6986d6b970af5c977e36a
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-10 23:12:18 +0200

    Update connect-four.py

--- connect-four.py
+++ connect-four.py
@@ -1,16 +1,1 @@
 class ConnectFour:
-    def __init__(self):
-        self.board = [[' ' for _ in range(7)] for _ in range(6)]
-        self.current_player = 'X'
-
-    def print_board(self):
-        for row in self.board:
-            print('|'.join(row))
-            print('-' * 13)
-        print(' 1 2 3 4 5 6 7')
-
-    def is_valid_move(self, column):
-        if column < 1 or column > 7:
-            return False
-        if self.board[0][column - 1] != ' ':
-            return False
