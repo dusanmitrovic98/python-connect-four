ce8de8dc7625c6c3e6312fe1958c6a409993e2a3
Dušan Mitrović (Dusan Mitrovic) <62337583+dusanmitrovic98@users.noreply.github.com>
2023-07-10 23:44:07 +0200

    Update connect-four.py

--- connect-four.py
+++ connect-four.py
@@ -32,3 +32,51 @@ class ConnectFour:
                 if self.board[row][col] == self.board[row][col+1] == self.board[row][col+2] == self.board[row][col+3] != ' ':
                     return True
 
+        # Check columns
+        for row in range(3):
+            for col in range(7):
+                if self.board[row][col] == self.board[row+1][col] == self.board[row+2][col] == self.board[row+3][col] != ' ':
+                    return True
+
+        # Check diagonals (top-left to bottom-right)
+        for row in range(3):
+            for col in range(4):
+                if self.board[row][col] == self.board[row+1][col+1] == self.board[row+2][col+2] == self.board[row+3][col+3] != ' ':
+                    return True
+
+        # Check diagonals (top-right to bottom-left)
+        for row in range(3):
+            for col in range(3, 7):
+                if self.board[row][col] == self.board[row+1][col-1] == self.board[row+2][col-2] == self.board[row+3][col-3] != ' ':
+                    return True
+
+        return False
+
+    def play(self):
+        print("Welcome to Connect Four!")
+        print("Player 1: X | Player 2: O")
+        print("Enter the column number (1-7) to make a move.")
+
+        while True:
+            self.print_board()
+
+            column = int(input(f"Player {self.current_player}, make your move: "))
+            if self.is_valid_move(column):
+                self.make_move(column)
+                if self.check_winner():
+                    self.print_board()
+                    print(f"Player {self.current_player} wins!")
+                    break
+                elif all(self.board[i][j] != ' ' for i in range(6) for j in range(7)):
+                    self.print_board()
+                    print("It's a draw!")
+                    break
+                else:
+                    self.switch_player()
+            else:
+                print("Invalid move. Try again.")
+
+
+game = ConnectFour()
+game.play()
+                
