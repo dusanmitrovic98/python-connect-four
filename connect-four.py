0dd3d976840b5f84357b22c1d2ca3ecf71b5d03c
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-10 23:26:20 +0200

    Update connect-four.py

--- connect-four.py
+++ connect-four.py
@@ -36,3 +36,4 @@ class ConnectFour:
         for row in range(3):
             for col in range(7):
             for col in range(7):
+                if self.board[row][col] == self.board[row+1][col] == self.board[row+2][col] == self.board[row+3][col] != ' ':
