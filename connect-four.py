b25d9f2b9096d6bc2f62fabe0324709e3aa3d7ca
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-17 18:53:04 +0200

    Update connect-four.py

--- connect-four.py
+++ connect-four.py
@@ -9,3 +9,4 @@ def create_board():
     # Create a 6x7 board filled with zeros (representing empty slots)
     return [[EMPTY] * COLUMN_COUNT for _ in range(ROW_COUNT)]
 
+def drop_piece(board, row, col, player):
